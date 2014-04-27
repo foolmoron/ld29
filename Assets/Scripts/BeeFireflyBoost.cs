80ac5c91af89992e3e74edeb2be72409a2436b16
Momin Khan <foolmoron@gmail.com>
2014-04-27 18:53:27 -0400

    Firefly bonuses done

--- Assets/Scripts/BeeFireflyBoost.cs
+++ Assets/Scripts/BeeFireflyBoost.cs
@@ -3,26 +3,33 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class BeeFireflyBoost : MonoBehaviour {
-
+	
 	public int FirefliesNeededForBoost = 3;
 	public int CurrentFirefliesInARow;
-
+	
 	HoneyBar honeyBar;
-
+	BorderFlasher borderFlasher;
+	bool alreadySetBorder;
+	
 	void Start() {
 		honeyBar = Object.FindObjectOfType<HoneyBar>();
+		borderFlasher = Object.FindObjectOfType<BorderFlasher>();
 	}
 	
 	void OnTriggerEnter2D(Collider2D other) {
 		if (other.name.Contains("Missed")){
 			CurrentFirefliesInARow = 0;
+			borderFlasher.SetTarget(0);
 		} else {
 			CurrentFirefliesInARow++;
+			borderFlasher.Flash();
 		}
 		Destroy(other.transform.root.gameObject);
-
+		
 		if (CurrentFirefliesInARow >= FirefliesNeededForBoost) {
 			honeyBar.EnableBoost();
+			if (CurrentFirefliesInARow == FirefliesNeededForBoost)
+				borderFlasher.SetTarget(0.75f);
 		} else {
 			honeyBar.DisableBoost();
 		}
---
+++ Assets/Scripts/BorderFlasher.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class BorderFlasher : MonoBehaviour {
+
+	public AnimationCurve TargetCurve;
+	public AnimationCurve FlashCurve;
+
+	SpriteRenderer sprite;
+	float originalAlpha;
+	float targetAlpha;
+	AnimationCurve currentCurve;
+	float animationTime;
+
+	void Start() {
+		sprite = GetComponent<SpriteRenderer>();
+	}
+
+	void Update() {
+		if (originalAlpha != targetAlpha) {
+			animationTime += Time.deltaTime;
+			var interp = currentCurve.Evaluate(animationTime);
+
+			var currentAlpha = Mathf.Lerp(originalAlpha, targetAlpha, interp);
+			var currentColor = sprite.color;
+			currentColor.a = currentAlpha;
+			sprite.color = currentColor;
+
+			if (interp > 1)
+				originalAlpha = targetAlpha;
+		}
+	}
+
+	public void SetOriginal(float alpha) {
+		originalAlpha = alpha;
+	}
+
+	public void SetTarget(float alpha) {
+		currentCurve = TargetCurve;
+		originalAlpha = sprite.color.a;
+		targetAlpha = alpha;
+		animationTime = 0;
+	}
+	
+	public void Flash() {
+		currentCurve = FlashCurve;
+		originalAlpha = sprite.color.a;
+		targetAlpha = originalAlpha + 0.5f;
+		animationTime = 0;
+	}
+}
