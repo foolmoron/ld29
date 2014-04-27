08167fd3a43baaae90c77e53080d16efc9992d42
Momin Khan <foolmoron@gmail.com>
2014-04-27 17:07:00 -0400

    Added firefly and boost system and such

---
+++ Assets/Scripts/BeeFireflyBoost.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BeeFireflyBoost : MonoBehaviour {
+
+	public int FirefliesNeededForBoost = 3;
+	public int CurrentFirefliesInARow;
+
+	HoneyBar honeyBar;
+
+	void Start() {
+		honeyBar = Object.FindObjectOfType<HoneyBar>();
+	}
+	
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.name.Contains("Missed")){
+			CurrentFirefliesInARow = 0;
+		} else {
+			CurrentFirefliesInARow++;
+		}
+		Destroy(other.transform.root.gameObject);
+
+		if (CurrentFirefliesInARow >= FirefliesNeededForBoost) {
+			honeyBar.EnableBoost();
+		} else {
+			honeyBar.DisableBoost();
+		}
+	}
+}
