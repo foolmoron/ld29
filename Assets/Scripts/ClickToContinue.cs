dc80398916a78a27fdc4b7bf961b70dcbdd2c07f
Momin Khan <foolmoron@gmail.com>
2014-04-27 19:52:29 -0400

    Added splash and fixed main scene

---
+++ Assets/Scripts/ClickToContinue.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ClickToContinue : MonoBehaviour {
+
+	public string SceneToGoTo;
+	
+	void Update() {
+		if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1)) {
+			Application.LoadLevel(SceneToGoTo);
+		}
+	}
+}
