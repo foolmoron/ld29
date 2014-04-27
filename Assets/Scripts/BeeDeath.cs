116246950697ae8e38be99b7a81627a0c7fa0d76
Momin Khan <foolmoron@gmail.com>
2014-04-26 21:37:11 -0400

    Added obstacles guard and bee death trigger system

---
+++ Assets/Scripts/BeeDeath.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BeeDeath : MonoBehaviour {
+
+	BeeGuard beeGuard;
+	List<Collider2D> electricsInside = new List<Collider2D>();
+
+	void Start() {
+		beeGuard = transform.root.GetComponentInChildren<BeeGuard>();
+	}
+	
+	void OnTriggerEnter2D(Collider2D other) {
+		electricsInside.Add(other);
+		AttemptDie();
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		electricsInside.Remove(other);
+	}
+
+	public void AttemptDie() {
+		if (beeGuard.IsInGuard || electricsInside.Count == 0)
+			return;
+
+		Debug.Log("DEAD'D");
+	}
+}
---
+++ Assets/Scripts/BeeGuard.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BeeGuard : MonoBehaviour {
+
+	BeeDeath beeDeath;
+	List<Collider2D> guardsInside = new List<Collider2D>();
+	public bool IsInGuard { get { return guardsInside.Count > 0; } }
+	
+	void Start() {
+		beeDeath = transform.root.GetComponentInChildren<BeeDeath>();
+	}
+	
+	void OnTriggerEnter2D(Collider2D other) {
+		guardsInside.Add(other);
+	}
+	
+	void OnTriggerExit2D(Collider2D other) {
+		guardsInside.Remove(other);
+		if (guardsInside.Count == 0)
+			beeDeath.AttemptDie();
+	}
+}
